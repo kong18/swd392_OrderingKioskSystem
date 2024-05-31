3da764f4632676138b54ac3a11a4e5f4789c47a3
sonhohuu <hhson365@gmail.com>
2024-05-31 13:32:03 +0700

    fix

--- OrderingKioskSystem.Application/Shipper/CreateShipper/CreateShipperCommandValidator.cs
+++ OrderingKioskSystem.Application/Shipper/CreateShipper/CreateShipperCommandValidator.cs
@@ -12,7 +12,6 @@ namespace OrderingKioskSystem.Application.Shipper.CreateShipper
         public CreateShipperCommandValidator()
         {
             RuleFor(x => x.Email).NotEmpty().EmailAddress();
-            RuleFor(x => x.Password).NotEmpty().MinimumLength(6);
             RuleFor(x => x.Role).NotEmpty().Equal("Shipper");
             RuleFor(x => x.ShipperName).NotEmpty();
             RuleFor(x => x.Phone).NotEmpty().Matches(@"^\+?\d+$").WithMessage("Invalid phone number format");
