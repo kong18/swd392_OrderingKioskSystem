7609825ad9d5168935b390594e513c63937acf61
kong18 <thanh2000cong20000@gmail.com>
2024-06-12 02:17:48 +0700

    fix

---
+++ OrderingKioskSystem.Application/Kiosk/Search/SearchKioskQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using OrderingKioskSystem.Application.Kiosk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWD.OrderingKioskSystem.Application.Kiosk.Get
+{
+    public class SearchKioskQuery : IRequest<List<KioskDTO>>
+    {
+        public string? Title { get; set; }
+        public string? Location { get; set; }
+    }
+}
---
+++ OrderingKioskSystem.Application/Menu/GetProductsByMenuId/GetProductsByMenuIdQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using MediatR;
+using OrderingKioskSystem.Application.Product;
+using OrderingKioskSystem.Domain.Repositories;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SWD.OrderingKioskSystem.Application.Menu.GetProductsByMenuId
+{
+    public class GetProductsByMenuIdQueryHandler : IRequestHandler<GetProductsByMenuIdQuery, List<ProductDTO>>
+    {
+        private readonly IProductMenuRepository _productMenuRepository;
+        private readonly IMapper _mapper;
+
+        public GetProductsByMenuIdQueryHandler(IProductMenuRepository productMenuRepository, IMapper mapper)
+        {
+            _productMenuRepository = productMenuRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<ProductDTO>> Handle(GetProductsByMenuIdQuery request, CancellationToken cancellationToken)
+        {
+            var productMenus = await _productMenuRepository
+                .FindAllAsync(pm => pm.MenuID == request.MenuId, cancellationToken);
+
+            var products = productMenus.Select(pm => pm.Product).ToList();
+
+            return _mapper.Map<List<ProductDTO>>(products);
+        }
+    }
+}
