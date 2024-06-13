abb68b993e3cb2fa4a01263d38258707b77f9893
kong18 <thanh2000cong20000@gmail.com>
2024-06-13 22:18:45 +0700

    manager

--- OrderingKioskSystem.Application/User/DeleteManagerCommand.cs
+++ OrderingKioskSystem.Application/User/DeleteManagerCommand.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SWD.OrderingKioskSystem.Application.VnPay
+namespace SWD.OrderingKioskSystem.Application.User
 {
-    internal class OrderDataModel
+    internal class DeleteManager
     {
     }
 }
