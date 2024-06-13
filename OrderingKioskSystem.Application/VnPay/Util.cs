03d51a542905eea7e850b8920ab88490ed27c44e
kong18 <thanh2000cong20000@gmail.com>
2024-06-13 22:12:46 +0700

    vn pay chua xong

---
+++ OrderingKioskSystem.Application/VnPay/Util.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWD.OrderingKioskSystem.Application.VnPay
+{
+    internal class Util
+    {
+    }
+}
--- OrderingKioskSystem.Application/VnPay/VnPayLibrary.cs
+++ OrderingKioskSystem.Application/VnPay/VnPayLibrary.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SWD.OrderingKioskSystem.Application.Util
+namespace SWD.OrderingKioskSystem.Application.VnPay
 {
-    internal class PayLib
+    internal class VnPayLibrary
     {
     }
 }
